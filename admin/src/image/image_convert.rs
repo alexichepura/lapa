a80a7d43982a48c756f26b1debd458a4af4b73d1
Alexi Chepura <a.chepura@gmail.com>
2023-07-05 09:33:39 +0300

    img arc inside ImageConvertConfig

--- admin/src/image/image_convert.rs
+++ admin/src/image/image_convert.rs
@@ -1,12 +1,11 @@
 use image::{imageops::FilterType, DynamicImage, ImageError, ImageFormat};
-use serde::{Deserialize, Serialize};
 use std::sync::Arc;
 
 use crate::settings::SettingsImages;
 
-#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Clone, Debug)]
 pub struct ImageConvertConfig {
-    // img: DynamicImage,
+    img: Arc<DynamicImage>,
     path: String,
     height: u32,
     width: u32,
@@ -31,57 +30,53 @@ impl From<&SettingsImages> for ConvertSettings {
     }
 }
 
-pub type ArcDynamicImage = Arc<DynamicImage>;
-
-pub fn create_image_variant(
-    img_decoded: ArcDynamicImage,
-    conf: ImageConvertConfig,
-) -> Result<(), ImageError> {
-    let variant = img_decoded.resize_to_fill(conf.width, conf.height, FilterType::Lanczos3);
+pub fn create_image_variant(conf: ImageConvertConfig) -> Result<(), ImageError> {
+    let variant = conf
+        .img
+        .resize_to_fill(conf.width, conf.height, FilterType::Lanczos3);
 
     variant.save_with_format(conf.path, ImageFormat::WebP)
 }
 
 pub fn create_image_variants(dynamic_image: DynamicImage, settings: &ConvertSettings, id: String) {
     let img_path = "img";
+    let arc = Arc::new(dynamic_image);
     let conf_large = ImageConvertConfig {
+        img: arc.clone(),
         path: format!("{img_path}/{id}-l.webp"),
         width: settings.hero_width,
         height: settings.hero_height,
     };
     let conf_large_retina = ImageConvertConfig {
+        img: arc.clone(),
         path: format!("{img_path}/{id}-l2.webp"),
         width: settings.hero_width * 2,
         height: settings.hero_height * 2,
     };
     let conf_small = ImageConvertConfig {
+        img: arc.clone(),
         path: format!("{img_path}/{id}-s.webp"),
         width: settings.thumb_width,
         height: settings.thumb_height,
     };
     let conf_small_retina = ImageConvertConfig {
+        img: arc.clone(),
         path: format!("{img_path}/{id}-s2.webp"),
         width: settings.thumb_width * 2,
         height: settings.thumb_height * 2,
     };
 
-    let arc: ArcDynamicImage = Arc::new(dynamic_image);
-    let arc_clone1 = arc.clone();
-    let arc_clone2 = arc.clone();
-    let arc_clone3 = arc.clone();
-    let arc_clone4 = arc.clone();
-
     let hadle_large = std::thread::spawn(move || {
-        let _result = create_image_variant(arc_clone1, conf_large);
+        let _result = create_image_variant(conf_large);
     });
     let hadle_large_retina = std::thread::spawn(move || {
-        let _result = create_image_variant(arc_clone2, conf_large_retina);
+        let _result = create_image_variant(conf_large_retina);
     });
     let hadle_small = std::thread::spawn(move || {
-        let _result = create_image_variant(arc_clone3, conf_small);
+        let _result = create_image_variant(conf_small);
     });
     let hadle_small_retina = std::thread::spawn(move || {
-        let _result = create_image_variant(arc_clone4, conf_small_retina);
+        let _result = create_image_variant(conf_small_retina);
     });
 
     hadle_large.join().unwrap();
