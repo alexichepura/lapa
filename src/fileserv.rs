4b22db0712dd9006c53c3bcfb14462efa4c80d93
Alexi Chepura <a.chepura@gmail.com>
2023-07-21 10:18:51 +0300

    MAX_AGE_MONTH for /img

--- src/fileserv.rs
+++ src/fileserv.rs
@@ -1,15 +1,16 @@
-cfg_if::cfg_if! { if #[cfg(feature = "ssr")] {
-    use axum::{
-        body::{boxed, Body, BoxBody},
-        extract::Extension,
-        response::{IntoResponse, Response as AxumResponse},
-        http::{Request, Response, StatusCode, Uri},
-    };
-    use std::sync::Arc;
-    use leptos::*;
-}}
+use axum::{
+    body::{boxed, Body, BoxBody},
+    extract::Extension,
+    http::{Request, Response, StatusCode, Uri},
+    response::{IntoResponse, Response as AxumResponse},
+};
+use http::HeaderValue;
+use leptos::*;
+use std::sync::Arc;
 
-#[cfg(feature = "ssr")]
+const MAX_AGE_MONTH: HeaderValue = HeaderValue::from_static("public, max-age=2592000");
+// const MAX_AGE_YEAR: HeaderValue = HeaderValue::from_static("public, max-age=31536000");
+
 pub async fn file_and_error_handler(
     uri: Uri,
     Extension(options): Extension<Arc<LeptosOptions>>,
@@ -20,16 +21,20 @@ pub async fn file_and_error_handler(
 
     let options = &*options;
     let uri_path = uri.path();
-    let (root, uri): (String, Uri) = if uri_path.starts_with("/img") {
+    let (root, uri, is_img): (String, Uri, bool) = if uri_path.starts_with("/img") {
         let uri_path = uri_path.replace("/img", "");
         let uri = uri_path.parse::<Uri>().unwrap();
-        ("img".to_string(), uri)
+        ("img".to_string(), uri, true)
     } else {
-        (options.site_root.clone(), uri.clone())
+        (options.site_root.clone(), uri.clone(), false)
     };
-    let res = get_static_file(uri, &root).await.unwrap();
+    let mut res = get_static_file(uri, &root).await.unwrap();
 
     if res.status() == StatusCode::OK {
+        if is_img {
+            res.headers_mut()
+                .insert(http::header::CACHE_CONTROL, MAX_AGE_MONTH);
+        }
         res.into_response()
     } else {
         let mut errors = Errors::default();
@@ -42,7 +47,6 @@ pub async fn file_and_error_handler(
     }
 }
 
-#[cfg(feature = "ssr")]
 async fn get_static_file(uri: Uri, root: &str) -> Result<Response<BoxBody>, (StatusCode, String)> {
     use tower::ServiceExt;
     use tower_http::services::ServeDir;
