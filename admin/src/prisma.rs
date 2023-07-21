7f95df03038ba616d6d6b9a0e9b0bed552d0b04c
Alexi Chepura <a.chepura@gmail.com>
2023-07-21 10:26:46 +0300

    prisma no need for cfg

--- admin/src/prisma.rs
+++ admin/src/prisma.rs
@@ -1,9 +1,7 @@
-#[cfg(feature = "ssr")]
+use leptos::{use_context, Scope, ServerFnError};
+
 pub type ArcPrisma = std::sync::Arc<prisma_client::db::PrismaClient>;
 
-#[cfg(feature = "ssr")]
-use leptos::{use_context, Scope, ServerFnError};
-#[cfg(feature = "ssr")]
 pub fn use_prisma(cx: Scope) -> Result<ArcPrisma, ServerFnError> {
     use_context::<ArcPrisma>(cx)
         .ok_or("Prisma missing.")
