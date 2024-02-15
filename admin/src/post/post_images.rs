dd3a0c26491baf18c22f98db73b4b9c615476b32
Alexi Chepura <a.chepura@gmail.com>
2024-02-15 21:15:51 +0300

    workaround strange error with html arrow and tracing

--- admin/src/post/post_images.rs
+++ admin/src/post/post_images.rs
@@ -218,7 +218,7 @@ where
                     }
                 >
 
-                    "⇐"
+                    "<"
                 </button>
                 {hero_view}
                 <button
@@ -230,7 +230,7 @@ where
                     }
                 >
 
-                    "⇒"
+                    ">"
                 </button>
             </footer>
         </figure>
